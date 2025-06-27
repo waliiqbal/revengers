395cb320c15dd1b06a08eed4b2705c746b7166c4
mfarazos <farazahmed.fa276@gmail.com>
2025-06-28 00:53:33 +0500

    done

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log(` Server running on: http://localhost:3000`);
+  await app.listen(3002);
+  console.log(` Server running on: http://localhost:3002`);
 }
 bootstrap();
